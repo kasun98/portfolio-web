189ca9762cb1f5c4f073357ae746f37430e15e4f
kasun98 <dewaka.lk@gmail.com>
2024-07-27 21:33:15 +0530

    f14

--- backportfolio/settings.py
+++ backportfolio/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise settings to enable compression and caching
